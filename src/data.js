7a3cdcb23b906e6dc31405e589cf8d18880e53a8
MatyasHroch <matyas.hroch01@upol.cz>
2024-08-13 20:28:55 +0200

    audio test

--- src/data.js
+++ src/data.js
@@ -15,56 +15,73 @@ export default {
       record_item_list: [
         {
           name: "Naivní malíř",
+          filename: "NaivniMalir",
         },
         {
           name: "Knoflík",
+          filename: "Knoflik",
         },
         {
           name: "Zikmund",
+          filename: "Zikmund",
         },
         {
           name: "Nahoru dolů",
+          filename: "NahoruDolu",
         },
         {
           name: "Svatební kar",
+          filename: "SvatebniKar",
         },
         {
           name: "Modlitba do skal",
+          filename: "ModlitbaDoSkal",
         },
         {
           name: "O podzimu",
+          filename: "OPodzimu",
         },
         {
           name: "Stříbrostín",
+          filename: "Strbrostn",
         },
       ],
       record_item_2_list: [
         {
           name: "Přípitek",
+          filename: "Pritek",
         },
         {
           name: "Na Jabloni",
+          filename: "NaJabloni",
         },
         {
           name: "Netopýr",
+          filename: "Netopyr",
         },
         {
           name: "Samotářská",
+          filename: "Samotarska",
         },
         {
           name: "Přetichá",
+          filename: "Preticha",
         },
         {
           name: "Pro rudu",
+          filename: "ProRudu",
         },
         {
           name: "Procházky",
+          filename: "Prochazky",
         },
         {
           name: "U kašky",
+          filename: "UKasky",
         },
         {
           name: "Lesanten",
+          filename: "Lesanten",
         },
       ],
     },
